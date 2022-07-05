445f19bd6289bade2a894bf4c72ff058f9e0b42d
Pradip Ganjale <pradipganjale277@gmail.com>
2022-07-05 17:19:59 +0530

    Angular1

---
+++ Day 10/Project1/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'project1';
+
+  fName = "Rahul";
+  earning =88888;
+  Status = "Active=yes";
+}
