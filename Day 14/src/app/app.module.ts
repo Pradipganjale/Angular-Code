9dd551d7d065904895eda0d757e8953699050a0f
Pradip Ganjale <pradipganjale277@gmail.com>
2022-07-10 13:59:15 +0530

    Angular4

---
+++ Day 14/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Assignment1Component } from './assignment1/assignment1.component';
+import { Assignment2Component } from './assignment2/assignment2.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Assignment1Component,
+    Assignment2Component,
+   
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
